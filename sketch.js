ceb072886b1e75661cb3d8f779f136488a8659d2
Melody Jan <melody0831@gmail.com>
2017-03-02 02:08:27 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+//EXAMPLE 01
+
+function setup() {
+    createCanvas(600, 900);
+    strokeWeight(4);
+}
+
+function draw() {
+    if(mouseIsPressed);
+    line(mouseX, mouseY, pmouseX, pmouseY);
+}
