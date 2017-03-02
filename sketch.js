f9f7f8b06c9cfc868b512a1b11d7d682289022f7
Melody Jan <melody0831@gmail.com>
2017-03-02 00:09:28 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,64 @@
-//EXAMPLE 01
+var angle = 0.0;
+var x = 0;
+var speed = 5;
+
+var a = 0;
 
 function setup() {
-    createCanvas(600, 900);
-    strokeWeight(4);
+    createCanvas(800, 800);
+    background(0);
 }
 
 function draw() {
-    if(mouseIsPressed);
-    line(mouseX, mouseY, pmouseX, pmouseY);
+    
+    redValue = random(255);
+    greenValue = random(255);
+    blueValue = random(255);
+    stroke(redValue, greenValue, blueValue);
+    
+    translate(width/2 + a, height/2 + a);
+    
+    rotate(angle);
+    angle += 0.1;
+    
+    
+if (mouseIsPressed == false) {
+
+beginShape();
+vertex(16.5, 10);
+vertex(19, 15);
+vertex(24, 15.5);
+vertex(20.5, 19);
+vertex(22, 24);
+vertex(16.5, 21.5);
+vertex(12, 24);
+vertex(13.5, 19);
+vertex(10, 15.5);
+vertex(15, 15);
+endShape(CLOSE);
+    
+    } else {
+       // background(255);
+        fill(0);
+        push();
+        translate(mouseX, mouseY);
+
+beginShape();
+vertex(16.5, 10);
+vertex(19, 15);
+vertex(24, 15.5);
+vertex(20.5, 19);
+vertex(22, 24);
+vertex(16.5, 21.5);
+vertex(12, 24);
+vertex(13.5, 19);
+vertex(10, 15.5);
+vertex(15, 15);
+endShape(CLOSE);
+        
+        pop();
+    }
+    
+   a = a+random(-8, 8); 
 }
+
